6c344c5ad1b7509d0c209e726a3517337edd7bf9
JakePhillip <panchee2548@gmail.com>
2026-08-20 01:10:42 +0700

    upload assets

--- Scripts/WorldScript.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-func _ready() -> void:
-	pass 
-	
-func _process(delta: float) -> void:
-	$"WorldEnvironment_(GlobalVolume)".environment.sky_rotation.y += 0.0002
-	if ($"WorldEnvironment_(GlobalVolume)".environment.sky_rotation.y >= 360):
-		$"WorldEnvironment_(GlobalVolume)".environment.sky_rotation.y = 0
